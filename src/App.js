18bccdd0a3ef8d8e6eee0a8c706559d529deef64
Pavel Mejinschii <pmezhinsky@bk.ru>
2024-10-19 18:04:58 +0300

    last

--- src/App.js
+++ src/App.js
@@ -1,25 +1,89 @@
 import logo from './logo.svg';
 import './App.css';
+import styles from "./app.module.css"
+import {useState} from "react";
+import Select from 'react-select';
+import {findAllByDisplayValue} from "@testing-library/react";
+import * as yup from 'yup'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [login, setLogin] = useState('')
+    const [loginError, setLoginError] = useState(null)
+    const [password, setPassword] = useState('')
+    const [passwordError, setPasswordError] = useState(null)
+
+
+    const onLoginChange = ({ target }) => {
+        setLogin(target.value)
+        let error = null
+        if (!/[\w_]$/.test(target.value)) {
+        error = 'недопустимые символы'
+        }else if(target.value.length>20) {
+        error = 'слишком много символов'
+        }
+        setLoginError(error)
+    }
+
+    const onPasswordChange = ( { target } ) => {
+        setPassword(target.value)
+        let error2 = null
+        if (password.length < 5) {
+            error2 = 'паорль должен содержать более 4 символов'
+        }
+        setPasswordError(error2)
+    }
+
+    const onLoginBlur = () => {
+        if (login.length < 3) {
+            setLoginError('Логин должен содержать более 3 символов')
+        }
+    }
+
+    const onPasswordBlur = () => {
+        if(password.length <5){
+            setPasswordError('паорль должен содержать более 4 символов')
+        }
+    }
+
+    const onSubmit = (event) => {
+        console.log(login)
+        event.preventDefault()
+    }
+
+    const onSubmitPass = (event) => {
+        console.log(password)
+        event.preventDefault()
+    }
+
+    return (
+        <div className={styles.app}>
+            <form onSubmit={onSubmit}>
+                {loginError && <div className={styles.errorLabel}>{loginError}</div>}
+                <input name='login'
+                       value={login}
+                       placeholder='login'
+                       type="text"
+                       onChange={onLoginChange}
+                       onBlur={onLoginBlur}
+                />
+
+            </form>
+            <form onSubmit={onSubmitPass}>
+                {loginError && <div className={styles.errorLabel}>{passwordError}</div>}
+                <input name='password'
+                       value={password}
+                       placeholder='password'
+                       type="text"
+                        onChange={onPasswordChange}
+                       onBlur={onPasswordBlur}
+                />
+
+            </form>
+            <button type='submit' disabled={loginError!==null || passwordError !==null}>Отрпавить форму</button>
+        </div>
+    )
 }
 
 export default App;
